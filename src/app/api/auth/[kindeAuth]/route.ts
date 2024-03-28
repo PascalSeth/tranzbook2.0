312b3dd661fcd2e528f4a19c0d2616567f039b57
PascalSeth <pascalelikem@gmail.com>
2024-03-28 16:33:52 +0000

    creation

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,3 +1,2 @@
-"use client"
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 export const GET = handleAuth();
--- src/app/api/auth/creation/route.ts
+++ src/app/api/auth/creation/route.ts
@@ -1,3 +1,4 @@
+"use client"
 import prisma from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
