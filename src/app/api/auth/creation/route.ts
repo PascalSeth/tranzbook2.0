5d7ab4b244241766d14f98ef3d8a30d529afc24e
PascalSeth <pascalelikem@gmail.com>
2024-03-29 05:15:27 +0000

    new

--- src/app/api/auth/creation/route.ts
+++ src/app/api/auth/creation/route.ts
@@ -1,10 +1,12 @@
-import prisma from "@/lib/db";
+import prisma from "@/app/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET(){
+    noStore()
     const {getUser}=getKindeServerSession();
-    const user= await getUser()
+    const user= await getUser();
 
     if(!user || user=== null || !user.id){
         throw new Error("Something went Wrong.... Sorry")
