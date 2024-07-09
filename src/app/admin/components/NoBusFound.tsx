da5af17f6af10ba3a2b652780c62b500bffc0374
PascalSeth <pascalelikem@gmail.com>
2024-07-09 11:27:42 +0000

    a[os

--- src/app/admin/components/NoBusFound.tsx
+++ src/app/admin/components/NoBusFound.tsx
@@ -13,7 +13,7 @@ function NoBusFound({}: Props) {
       </div>
       <div className="w-full lg:w-1/2">
         <h1 className="py-4 text-3xl lg:text-4xl font-extrabold text-gray-800">No Buses Found</h1>
-        <p className="py-4 text-base text-gray-800">Unfortunately, we couldn't find any buses matching your search criteria. This might be due to an incorrect link or the absence of available buses.</p>
+        <p className="py-4 text-base text-gray-800">Unfortunately, we couldn&apos;t find any buses matching your search criteria. This might be due to an incorrect link or the absence of available buses.</p>
         <p className="py-2 text-base text-gray-800">We apologize for the inconvenience. Please return to our homepage to continue your search or explore other options.</p>
       <Link href='/'>
         <button className="w-full lg:w-auto my-4 border rounded-md px-1 sm:px-16 py-5 bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">Return to Homepage</button>
